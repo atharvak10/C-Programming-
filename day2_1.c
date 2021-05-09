584dcb8cdd27a461855089e38222477cb922bbac
atharvak10 <83536357+atharvak10@users.noreply.github.com>
2021-05-09 11:40:26 +0530

    Add files via upload

---
+++ day2_1.c
@@ -0,0 +1,42 @@
+ // documentation / Comments
+// (non executable statements)
+
+// 1. Single line Comments (//)
+// 2. Multi Line Comments (/*...... */)
+ 
+/*
+ program to write "Welcome OM13 batch" on output screen.
+
+ Author:Akshita, Sunbeam
+ Date : 19th November 2020
+
+*/
+
+// Header File Inclusion Area 
+//library files 
+
+//#inlcude (include the file stdio.h in my program)
+//preprocessing
+
+#include<stdio.h>
+//entry point function
+int main(void)
+{
+
+        printf("Welcome OM13 Batch");  
+        return 0;     
+            //0 means it is successfully exited 
+            //0 EXIT_SUCCESS
+}
+//defining main() function
+//main()function body / SCOPE 
+
+
+
+//Convertor (High Level lang into machine level language)
+//COMPILER
+
+
+//1. Edit the program
+//2. Compile the program ( gcc programname.c)
+//3. Execute the program (./a.exe  OR .\a.exe  OR ./day2_1.exe)
